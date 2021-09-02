5df49805ab1b5e6e7b0b775c0322db4b225e2998
GALVES <GALVES@GALVES-7410.br.oracle.com>
2021-09-02 10:52:30 -0300

    final

--- src/main/java/com/example/b3kafkaproducerapi/MessageAPI.java
+++ src/main/java/com/example/b3kafkaproducerapi/MessageAPI.java
@@ -1,5 +1,7 @@
 package com.example.b3kafkaproducerapi;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,28 +13,31 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/response")
+@RequestMapping("/stream")
 
 public class MessageAPI {
 
-        @PostMapping("/postbody")
-        public String postBody(@RequestBody String fullName) {
-            return "Hello " + fullName;
-        }
 
     @PostMapping(
-            value = "/postbody1",
+            value = "/send",
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public String postBody(@RequestBody Message message) {
         Message persistedMessage = message;
 
         Producer simpleProducer = new Producer();
-        String topic = "topic";
-        String msg = "message";
-        ProducerRecord<Integer, Message> data = new ProducerRecord<>(topic, 8, persistedMessage);
-        simpleProducer.producer.send(data);
-        simpleProducer.producer.close();
+        String topic = "stream";
+        ObjectMapper objectMessage = new ObjectMapper();
+        try {
+            String messageToJson = objectMessage.writeValueAsString(persistedMessage);
+            ProducerRecord<Integer, String> data = new ProducerRecord<>(topic, 8, messageToJson);
+            simpleProducer.producer.send(data);
+            simpleProducer.producer.close();
+        }
+        catch (JsonProcessingException e)
+        {
+            e.printStackTrace();
+        }
         return "Message sent";
     }
 
--- src/main/java/com/example/b3kafkaproducerapi/Producer.java
+++ src/main/java/com/example/b3kafkaproducerapi/Producer.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 
 public class Producer {
 
-    public KafkaProducer<Integer, Message> producer;
+    public KafkaProducer<Integer, String> producer;
     private final Properties properties = new Properties();
 
     public Producer() {
@@ -16,11 +16,11 @@ public class Producer {
         properties.put("security.protocol", "SASL_SSL");
         properties.put("sasl.mechanism", "PLAIN");
         properties.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"oraclepocb3/oracleidentitycloudservice/gustavo.alves@oracle.com/ocid1.streampool.oc1.sa-saopaulo-1.amaaaaaaupwapsqanhzrlwefuoy3qnvnalwvy37w5gipwu4sukwylzhdcyfa\" password=\"P1QPs5fF7P6r9D<:B>Ve\";");
-        properties.put("acks", "all");
-        properties.put("retries", 0);
-        properties.put("batch.size", 16384);
-        properties.put("linger.ms", 1);
-        properties.put("buffer.memory", 33554432);
+//        properties.put("acks", "all");
+//        properties.put("retries", 0);
+//        properties.put("batch.size", 16384);
+//        properties.put("linger.ms", 1);
+//        properties.put("buffer.memory", 33554432);
         properties.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
         properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         producer = new KafkaProducer<>(properties);
